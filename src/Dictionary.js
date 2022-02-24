d749c569877b652957e53d1f2becc58f7235f777
Nata <anblurry@gmail.com>
2022-02-24 22:45:40 +0100

    Feat: Search Bar
    Add search bar form and function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
   function search(event) {
     event.preventDefault();
-    alert("Searching...");
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordUpdate(event) {
+    setKeyword(event.target.value);
   }
 
   return (
     <div className="SearchContent">
       <form onSubmit={search}>
-        <input type="search" placeholder="Type your word" autoFocus={true} />
+        <input
+          type="search"
+          placeholder="Type your word"
+          autoFocus={true}
+          onChange={handleKeywordUpdate}
+        />
       </form>
     </div>
   );
