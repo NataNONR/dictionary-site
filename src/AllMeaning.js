87114df132188fc87c1ca718242638fad9de81e2
Nata <anblurry@gmail.com>
2022-02-25 17:17:58 +0100

    Feat: Synonym
    Add functions and components to show synonyms of the word

--- src/AllMeaning.js
+++ src/AllMeaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function AllMeaning(props) {
   console.log(props.meaning);
@@ -11,6 +12,7 @@ export default function AllMeaning(props) {
             {definition.definition}
             <br />
             <em>{definition.example}</em>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
