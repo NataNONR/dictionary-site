1af5d6ce4da5fe016f076dfb4f3e2bad3773ad28
Nata <anblurry@gmail.com>
2022-03-01 15:22:53 +0100

    Feat: Fix Hyperlink
    Add rel="noreferrer" to hyperlink

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ import "./Phonetics.css";
 export default function Phonetics(props) {
   return (
     <div className="AllPhonetics">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         🔉
       </a>
       <span className="text">{props.phonetic.text}</span>
